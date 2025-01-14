2409d8946f802f83997002789c2281cfbcf190f5
Joy Sutrdhar <sutradharjoy2006@gmail.com>
2025-01-14 21:54:15 +0600

    Create README.py

---
+++ What's Your Name?/README.py
@@ -0,0 +1,43 @@
+# HackerRank Problem: What's Your Name?
+
+## Problem Statement
+
+You are given the first name and last name of a person. Your task is to print a string that says:
+
+```
+Hello <First Name> <Last Name>! You just delved into python.
+```
+
+### Function Description
+
+Complete the function `print_full_name` in the editor below.
+
+#### `print_full_name` has the following parameters:
+- **string first**: The first name of the person.
+- **string last**: The last name of the person.
+
+### Input Format
+The first line contains the first name, **first**.
+The second line contains the last name, **last**.
+
+### Constraints
+- The length of the first and last names are each ≤ 10.
+
+### Output Format
+Print the output as described above.
+
+### Sample Input 0
+```
+Ross
+Taylor
+```
+
+### Sample Output 0
+```
+Hello Ross Taylor! You just delved into python.
+```
+
+### Explanation 0
+The input is combined into the string:
+```
+Hello Ross Taylor! You just delved into python.
